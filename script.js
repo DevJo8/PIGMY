29a859489e9f86aad532126005ae7e8d8cd374b2
DevJo8 <152252543+DevJo8@users.noreply.github.com>
2025-07-14 06:35:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function copyCa() {
-	const ca = "J4tKfGfD6obuNwocH3MskyF2LyLwdB9r7658qSe1pump";
+	const ca = "SOON";
 	const caTooltip = document.querySelector(".ca-tooltip");
 	navigator.clipboard
 		.writeText(ca)
